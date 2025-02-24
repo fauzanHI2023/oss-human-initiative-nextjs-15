faa0967668cd885dafa65a61094b29f47ae6cd66
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2025-02-24 16:37:42 +0700

    position mobile menu

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -287,7 +287,6 @@ const Navbar = (props: Props) => {
   );
 
   const container: React.CSSProperties = {
-    position: "relative",
     justifyContent: "flex-start",
     alignItems: "stretch",
     flex: 1,
@@ -306,7 +305,7 @@ const Navbar = (props: Props) => {
   const background: React.CSSProperties = {
     position: "absolute",
     top: 0,
-    left: 0,
+    right: 90,
     bottom: 0,
     width: 300,
   };
@@ -384,7 +383,7 @@ const Navbar = (props: Props) => {
             href="/"
             className={`${
               isHome ? "bg-logo-blue" : "bg-logo-blue"
-            } w-32 h-12 bg-no-repeat bg-contain flex title-font font-medium items-center text-gray-900 mb-0`}
+            } hidden sm:flex w-32 h-12 bg-no-repeat bg-contain title-font font-medium items-center text-gray-900 mb-0`}
           ></Link>
           <ul
             className={`hidden sm:flex flex-row relative mt-4 w-auto px-4 gap-x-6`}
@@ -404,44 +403,189 @@ const Navbar = (props: Props) => {
               </li>
             ))}
           </ul>
-          <div style={container} className="sm:hidden flex">
-            <motion.nav
-              initial={false}
-              animate={isOpen ? "open" : "closed"}
-              custom={height}
-              ref={containerRef}
-              style={nav}
-              className="flex"
-            >
-              <motion.div style={background} variants={sidebarVariants} className="bg-gradient-to-r from-cyan-500 to-blue-500"/>
-              <motion.ul
-                style={list}
-                variants={navVariants}
-                className={`flex flex-col`}
+          <div className="sm:hidden w-full flex flex-row justify-between ">
+            <div style={container} className="sm:hidden flex">
+              <motion.nav
+                initial={false}
+                animate={isOpen ? "open" : "closed"}
+                custom={height}
+                ref={containerRef}
+                style={nav}
+                className="flex"
               >
-                {menuItems.map((item) => (
-                  <motion.li
-                    style={listItem}
-                    variants={itemVariants}
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.95 }}
-                    key={item.id}
-                    className="relative group text-base font-normal pb-2"
-                  >
-                    <Link
-                      href={item.url}
-                      className="flex flex-row dark:hover:text-slate-200 dark:text-slate hover:text-slate-700 hover:font-semibold transition duration-200 ease-in text-slate-200 text-base font-semibold"
+                <motion.div
+                  style={background}
+                  variants={sidebarVariants}
+                  className="bg-gradient-to-r from-sky-500 to-sky-800"
+                />
+                <motion.ul
+                  style={list}
+                  variants={navVariants}
+                  className={`flex flex-col`}
+                >
+                  {menuItems.map((item) => (
+                    <motion.li
+                      style={listItem}
+                      variants={itemVariants}
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.95 }}
+                      key={item.id}
+                      className="relative group text-base font-normal pb-2"
                     >
-                      {item.label} <ChevronDown className="text-sm w-4" />
-                    </Link>
-                    {item.subMenu && <SubMenu items={item.subMenu} />}
+                      <Link
+                        href={item.url}
+                        className="flex flex-row dark:hover:text-slate-200 dark:text-slate hover:text-slate-700 hover:font-semibold transition duration-200 ease-in text-slate-200 text-base font-semibold"
+                      >
+                        {item.label} <ChevronDown className="text-sm w-4" />
+                      </Link>
+                      {item.subMenu && <SubMenu items={item.subMenu} />}
                     </motion.li>
-                ))}
-              </motion.ul>
-              <MenuToggle toggle={() => setIsOpen(!isOpen)} />
-            </motion.nav>
+                  ))}
+                </motion.ul>
+                <MenuToggle toggle={() => setIsOpen(!isOpen)} />
+              </motion.nav>
+            </div>
+            <Link
+              href="/"
+              className={`${
+                isHome ? "bg-logo-blue" : "bg-logo-blue"
+              } sm:hidden flex w-32 h-12 bg-no-repeat bg-contain title-font font-medium items-center text-gray-900 mb-0`}
+            ></Link>
+            <div className="flex sm:hidden flex-row gap-x-4 justify-center items-start">
+              <div className="flex shrink-0 items-center gap-x-4 hidden">
+                <DropdownMenu>
+                  <DropdownMenuTrigger
+                    asChild
+                    className="z-[99999] bg-slate-200 text-slate-700 dark:text-white hover:bg-slate-300 dark:bg-slate-700 focus:outline-none relative"
+                  >
+                    <Button size="icon">
+                      <Sun className="h-[1.2rem] text-slate-700 dark:text-slate-100 w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                      <span className="sr-only">Toggle theme</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                      Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                      Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                      System
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <div
+                  className="relative flex justify-center items-center bg-slate-200 dark:bg-slate-700 py-2 px-2 rounded"
+                  onMouseEnter={() => setShowCartDropdown(true)}
+                  onMouseLeave={() => setShowCartDropdown(false)}
+                >
+                  <AiOutlineShoppingCart className="text-2xl cursor-pointer" />
+                  {cartCount > 0 && (
+                    <span className="absolute top-0 right-0 bg-amber-400 dark:bg-red-500 bg-red-500 text-white text-xs font-bold w-4 h-4 flex items-center justify-center rounded-full">
+                      {cartCount}
+                    </span>
+                  )}
+                  {showCartDropdown && (
+                    <div
+                      className="fixed inset-0 bg-black bg-opacity-75 z-10 top-0 left-0 transition-opacity duration-200 ease-in w-full h-full"
+                      style={{ top: "84px" }}
+                    >
+                      <div
+                        className="absolute top-0 right-0 bg-gray-50 dark:bg-slate-900 w-96 rounded-xl border-slate-100 shadow-2xl"
+                        style={{ right: "4rem", top: "-20px" }}
+                      >
+                        <div className="flex flex-row justify-between items-center border-b">
+                          <h3 className="text-base font-semibold p-4">
+                            Cart{" "}
+                            <span className="text-sm pl-2">{cartCount}</span>
+                          </h3>
+                          {cartItems.length > 0 && (
+                            <div className="p-4 border-t">
+                              <Link href="/checkout">
+                                <button className="w-full text-sky-600">
+                                  See All
+                                </button>
+                              </Link>
+                            </div>
+                          )}
+                        </div>
+                        <ul className="p-4 flex flex-col gap-y-5">
+                          {cartDetails.length > 0 ? (
+                            cartDetails.map((item) => (
+                              <li
+                                key={item.campaign_id}
+                                className="flex flex-row w-full mb-2 justify-between items-center"
+                              >
+                                <Link href={`/campaign/${item.slug}`}>
+                                  <div className="flex items-center gap-4">
+                                    <Image
+                                      src={`https://cdnx.human-initiative.org/image/${item.campaign_img}`}
+                                      alt={
+                                        item.campaign_name || "Campaign Image"
+                                      }
+                                      width={100}
+                                      height={100}
+                                      className="w-12 h-12 rounded-md object-cover"
+                                    />
+                                    <div>
+                                      <h4 className="text-sm font-medium">
+                                        {item.campaign_name}
+                                      </h4>
+                                      <p className="text-xs text-slate-700 dark:text-slate-300 ">
+                                        {formatPrice(item.amount)}
+                                      </p>
+                                    </div>
+                                  </div>
+                                </Link>
+                              </li>
+                            ))
+                          ) : (
+                            <li className="text-center text-gray-500">
+                              Keranjang kosong.
+                            </li>
+                          )}
+                        </ul>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <Suspense fallback="loading">
+                <div className="flex flex-wrap items-center text-base justify-center gap-6">
+                  {session ? (
+                    <Fragment>
+                      <Link
+                        href="/dashboard"
+                        className={`${isHome ? "" : ""} flex`}
+                      >
+                        Hi, {user?.full_name}
+                      </Link>
+                      <ButtonLogout />
+                    </Fragment>
+                  ) : (
+                    <div className="flex flex-row justify-center items-center">
+                      <button
+                        onClick={() => setShowModal(true)}
+                        className="h-full font-semibold inline-flex items-center bg-sky-600 border-0 py-2 px-4 focus:outline-none rounded-xl text-sm text-white mt-0 sm:mt-0 mr-2"
+                      >
+                        Log In <LogIn className="pl-2 w-8" />
+                      </button>
+                      <p>|</p>
+                      <Link
+                        href="/register"
+                        className="h-full inline-flex items-center  border-0 py-2 px-2 focus:outline-none rounded text-sm text-slate-600 dark:text-white mt-0 sm:mt-0"
+                      >
+                        <BookUser />
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </Suspense>
+            </div>
           </div>
-          <div className="flex flex-row gap-x-4 ">
+          <div className="hidden sm:flex flex-row gap-x-4 ">
             <div className="flex shrink-0 items-center gap-x-4">
               <DropdownMenu>
                 <DropdownMenuTrigger
